3b89a07b763cffab5d44e143db5802019312ada6
eran.heres@gmail.com <eran.heres@gmail.com>
2024-04-06 20:12:40 +0300

    more costumizations

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+# TODOs
+[x] Make half screen up/down by Alt-jk
+[x] Save few lines on top and on the bottom while scrolling 
+[x] Auto-save file
+[x] Auto-save session
+
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Automatically update the buffer when leaving insert mode
+local auto_update_grp = vim.api.nvim_create_augroup("auto_update_buffer", { clear = true })
+vim.api.nvim_create_autocmd({"InsertLeave", "TextChanged"}, {
+    group = auto_update_grp,
+    pattern = "*",
+    callback = function()
+        vim.cmd('silent! update')
+    end,
+})
+
