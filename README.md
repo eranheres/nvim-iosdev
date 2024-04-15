0832f22ed02d2cd00999ed951f1768b72d532e89
eran.heres@gmail.com <eran.heres@gmail.com>
2024-04-15 16:13:52 +0300

    changes

--- README.md
+++ README.md
@@ -7,5 +7,9 @@ Refer to the [documentation](https://lazyvim.github.io/installation) to get star
 [x] Make half screen up/down by Alt-jk
 [x] Save few lines on top and on the bottom while scrolling 
 [x] Auto-save file
+[ ] Make Alt+Space to show recommendations
 [x] Auto-save session
+[x] Check why lazygit is not working
+[x] Check why formatter is missing (<leader>cf)
+[x] Check why md files are not properly supported
 
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- xcodebuild keymaps
+
 vim.keymap.set("n", "<leader>X", "<cmd>XcodebuildPicker<cr>", { desc = "Show Xcodebuild Actions" })
 vim.keymap.set("n", "<leader>xf", "<cmd>XcodebuildProjectManager<cr>", { desc = "Show Project Manager Actions" })
 
@@ -26,3 +28,18 @@ vim.keymap.set("n", "<leader>xq", "<cmd>Telescope quickfix<cr>", { desc = "Show 
 
 vim.keymap.set("n", "<leader>xx", "<cmd>XcodebuildQuickfixLine<cr>", { desc = "Quickfix Line" })
 vim.keymap.set("n", "<leader>xa", "<cmd>XcodebuildCodeActions<cr>", { desc = "Show Code Actions" })
+
+-- remap pageup/pagedown
+vim.api.nvim_set_keymap("n", "<C-u>", "5<C-u>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-d>", "5<C-d>", { noremap = true, silent = true })
+
+-- debugging
+vim.keymap.set("n", "<F5>", function()
+	require("dap").continue()
+end)
+vim.keymap.set("n", "<F8>", function()
+	require("dap").step_over()
+end)
+vim.keymap.set("n", "<F7>", function()
+	require("dap").step_into()
+end)
